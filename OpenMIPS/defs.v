38804ee93d618f99f4d5ff45c462d9eb70f698e6
haoozi <lkwywhy@163.com>
2015-05-07 22:36:04 +0800

    add div module \& test asm

--- OpenMIPS/defs.v
+++ OpenMIPS/defs.v
@@ -179,3 +179,19 @@
 `define RegNum				32
 `define RegNumLog2			5
 `define NOPRegAddr			5'b00000
+
+
+
+
+//vars of div
+`define DivFree					2'b00
+`define DivByZero				2'b01
+`define DivOn					2'b10
+`define DivEnd					2'b11
+
+
+`define DivStart				1'b1
+`define DivStop					1'b0
+
+`define DivResultReady			1'b1
+`define DivResultNotReady		1'b0
