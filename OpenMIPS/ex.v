02a7bea52826e00343d5bb8ecca5278f49e002cb
haoozi <lkwywhy@163.com>
2015-05-09 18:03:34 +0800

    Almost Finish Core

--- OpenMIPS/ex.v
+++ OpenMIPS/ex.v
@@ -1,103 +1,90 @@
-//////////////////////////////////////////////////////////////////////
-////                                                              ////
-//// Copyright (C) 2014 leishangwen@163.com                       ////
-////                                                              ////
-//// This source file may be used and distributed without         ////
-//// restriction provided that this copyright statement is not    ////
-//// removed from the file and that any derivative work contains  ////
-//// the original copyright notice and the associated disclaimer. ////
-////                                                              ////
-//// This source file is free software; you can redistribute it   ////
-//// and/or modify it under the terms of the GNU Lesser General   ////
-//// Public License as published by the Free Software Foundation; ////
-//// either version 2.1 of the License, or (at your option) any   ////
-//// later version.                                               ////
-////                                                              ////
-//// This source is distributed in the hope that it will be       ////
-//// useful, but WITHOUT ANY WARRANTY; without even the implied   ////
-//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ////
-//// PURPOSE.  See the GNU Lesser General Public License for more ////
-//// details.                                                     ////
-////                                                              ////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-// Module:  ex
-// File:    ex.v
-// Author:  Lei Silei
-// E-mail:  leishangwen@163.com
-// Description: 执行阶段
-// Revision: 1.0
-//////////////////////////////////////////////////////////////////////
-
 `include "defines.v"
 
 module ex(
 
-	input wire										rst,
+	input wire						rst,
+
+	input wire[`AluOpBus]			aluop_i,
+	input wire[`AluSelBus]			alusel_i,
+	input wire[`RegBus]				reg1_i,
+	input wire[`RegBus]				reg2_i,
+	input wire[`RegAddrBus]			wd_i,
+	input wire 						wreg_i,
+	input wire[`RegBus]				inst_i,
+	input wire[31:0]				excepttype_i,
+	input wire[`RegBus]				current_inst_address_i,
+
+	input wire[`RegBus]				hi_i,
+	input wire[`RegBus]				lo_i,
+
+	input wire[`RegBus]				wb_hi_i,
+	input wire[`RegBus]				wb_lo_i,
+	input wire 						wb_whilo_i,
+
+	input wire[`RegBus]				mem_hi_i,
+	input wire[`RegBus]				mem_lo_i,
+	input wire 						mem_whilo_i,
+
+	input wire[`DoubleRegBus]		hilo_temp_i,
+	input wire[1:0]					cnt_i,
+
+	input wire[`DoubleRegBus]		div_result_i,
+	input wire 						div_ready_i,
+
+	input wire[`RegBus]				link_address_i,
+	input wire 						is_in_delayslot_i,	
+
+	input wire 						mem_cp0_reg_we,
+	input wire[4:0]					mem_cp0_reg_write_addr,
+	input wire[`RegBus]				mem_cp0_reg_data,
 	
-	//送到执行阶段的信息
-	input wire[`AluOpBus]         aluop_i,
-	input wire[`AluSelBus]        alusel_i,
-	input wire[`RegBus]           reg1_i,
-	input wire[`RegBus]           reg2_i,
-	input wire[`RegAddrBus]       wd_i,
-	input wire                    wreg_i,
+	input wire 						wb_cp0_reg_we,
+	input wire[4:0]					wb_cp0_reg_write_addr,
+	input wire[`RegBus]				wb_cp0_reg_data,
 
-	//HI、LO寄存器的值
-	input wire[`RegBus]           hi_i,
-	input wire[`RegBus]           lo_i,
+	input wire[`RegBus]				cp0_reg_data_i,
+	output reg[4:0]					cp0_reg_read_addr_o,
 
-	//回写阶段的指令是否要写HI、LO，用于检测HI、LO的数据相关
-	input wire[`RegBus]           wb_hi_i,
-	input wire[`RegBus]           wb_lo_i,
-	input wire                    wb_whilo_i,
+	output reg 						cp0_reg_we_o,
+	output reg[4:0]					cp0_reg_write_addr_o,
+	output reg[`RegBus]				cp0_reg_data_o,
 	
-	//访存阶段的指令是否要写HI、LO，用于检测HI、LO的数据相关
-	input wire[`RegBus]           mem_hi_i,
-	input wire[`RegBus]           mem_lo_i,
-	input wire                    mem_whilo_i,
+	output reg[`RegAddrBus]			wd_o,
+	output reg 						wreg_o,
+	output reg[`RegBus]				wdata_o,
 
-	input wire[`DoubleRegBus]     hilo_temp_i,
-	input wire[1:0]               cnt_i,
-
-	//与除法模块相连
-	input wire[`DoubleRegBus]     div_result_i,
-	input wire                    div_ready_i,
-
-	//是否转移、以及link address
-	input wire[`RegBus]           link_address_i,
-	input wire                    is_in_delayslot_i,	
+	output reg[`RegBus]				hi_o,
+	output reg[`RegBus]				lo_o,
+	output reg 						whilo_o,
 	
-	output reg[`RegAddrBus]       wd_o,
-	output reg                    wreg_o,
-	output reg[`RegBus]						wdata_o,
+	output reg[`DoubleRegBus]		hilo_temp_o,
+	output reg[1:0]					cnt_o,
 
-	output reg[`RegBus]           hi_o,
-	output reg[`RegBus]           lo_o,
-	output reg                    whilo_o,
+	output reg[`RegBus]				div_opdata1_o,
+	output reg[`RegBus]				div_opdata2_o,
+	output reg 						div_start_o,
+	output reg 						signed_div_o,
+
+	output wire[`AluOpBus]			aluop_o,
+	output wire[`RegBus]			mem_addr_o,
+	output wire[`RegBus]			reg2_o,
 	
-	output reg[`DoubleRegBus]     hilo_temp_o,
-	output reg[1:0]               cnt_o,
+	output wire[31:0]				excepttype_o,
+	output wire 					is_in_delayslot_o,
+	output wire[`RegBus]			current_inst_address_o,
 
-	output reg[`RegBus]           div_opdata1_o,
-	output reg[`RegBus]           div_opdata2_o,
-	output reg                    div_start_o,
-	output reg                    signed_div_o,
-
-	output reg										stallreq       			
-	
+	output reg						stallreq	
 );
 
 	reg[`RegBus] logicout;
 	reg[`RegBus] shiftres;
 	reg[`RegBus] moveres;
 	reg[`RegBus] arithmeticres;
-	reg[`DoubleRegBus] mulres;	
+	reg[`DoubleRegBus] mulres;
 	reg[`RegBus] HI;
 	reg[`RegBus] LO;
 	wire[`RegBus] reg2_i_mux;
-	wire[`RegBus] reg1_i_not;	
+	wire[`RegBus] reg1_i_not;
 	wire[`RegBus] result_sum;
 	wire ov_sum;
 	wire reg1_eq_reg2;
@@ -106,9 +93,18 @@ module ex(
 	wire[`RegBus] opdata2_mult;
 	wire[`DoubleRegBus] hilo_temp;
 	reg[`DoubleRegBus] hilo_temp1;
-	reg stallreq_for_madd_msub;			
+	reg stallreq_for_madd_msub;
 	reg stallreq_for_div;
-			
+	reg trapassert;
+	reg ovassert;
+
+	assign aluop_o = aluop_i;
+	assign mem_addr_o = reg1_i + {{16{inst_i[15]}},inst_i[15:0]};
+	assign reg2_o = reg2_i;
+	assign excepttype_o = {excepttype_i[31:12],ovassert,trapassert,excepttype_i[9:8],8'h00};
+	assign is_in_delayslot_o = is_in_delayslot_i;
+	assign current_inst_address_o = current_inst_address_i;
+
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			logicout <= `ZeroWord;
@@ -130,8 +126,8 @@ module ex(
 					logicout <= `ZeroWord;
 				end
 			endcase
-		end    //if
-	end      //always
+		end
+	end
 
 	always @ (*) begin
 		if(rst == `RstEnable) begin
@@ -152,26 +148,29 @@ module ex(
 					shiftres <= `ZeroWord;
 				end
 			endcase
-		end    //if
-	end      //always
+		end
+	end 
 
 	assign reg2_i_mux = ((aluop_i == `EXE_SUB_OP) || (aluop_i == `EXE_SUBU_OP) ||
-											 (aluop_i == `EXE_SLT_OP) ) 
+											(aluop_i == `EXE_SLT_OP)|| (aluop_i == `EXE_TLT_OP) ||
+											(aluop_i == `EXE_TLTI_OP) || (aluop_i == `EXE_TGE_OP) ||
+											(aluop_i == `EXE_TGEI_OP)) 
 											 ? (~reg2_i)+1 : reg2_i;
 
-	assign result_sum = reg1_i + reg2_i_mux;										 
-
+	assign result_sum = reg1_i + reg2_i_mux;
 	assign ov_sum = ((!reg1_i[31] && !reg2_i_mux[31]) && result_sum[31]) ||
 									((reg1_i[31] && reg2_i_mux[31]) && (!result_sum[31]));  
 									
-	assign reg1_lt_reg2 = ((aluop_i == `EXE_SLT_OP)) ?
-												 ((reg1_i[31] && !reg2_i[31]) || 
-												 (!reg1_i[31] && !reg2_i[31] && result_sum[31])||
-			                   (reg1_i[31] && reg2_i[31] && result_sum[31]))
-			                   :	(reg1_i < reg2_i);
-  
-  assign reg1_i_not = ~reg1_i;
-							
+	assign reg1_lt_reg2 = ((aluop_i == `EXE_SLT_OP) || (aluop_i == `EXE_TLT_OP) ||
+							(aluop_i == `EXE_TLTI_OP) || (aluop_i == `EXE_TGE_OP) ||
+							(aluop_i == `EXE_TGEI_OP)) ?
+							((reg1_i[31] && !reg2_i[31]) || 
+							(!reg1_i[31] && !reg2_i[31] && result_sum[31])||
+							(reg1_i[31] && reg2_i[31] && result_sum[31]))
+							:(reg1_i < reg2_i);
+
+	assign reg1_i_not = ~reg1_i;
+
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			arithmeticres <= `ZeroWord;
@@ -219,16 +218,48 @@ module ex(
 		end
 	end
 
-  //取得乘法操作的操作数，如果是有符号除法且操作数是负数，那么取反加一
+	always @ (*) begin
+		if(rst == `RstEnable) begin
+			trapassert <= `TrapNotAssert;
+		end else begin
+			trapassert <= `TrapNotAssert;
+			case (aluop_i)
+				`EXE_TEQ_OP, `EXE_TEQI_OP:		begin
+					if( reg1_i == reg2_i ) begin
+						trapassert <= `TrapAssert;
+					end
+				end
+				`EXE_TGE_OP, `EXE_TGEI_OP, `EXE_TGEIU_OP, `EXE_TGEU_OP:		begin
+					if( ~reg1_lt_reg2 ) begin
+						trapassert <= `TrapAssert;
+					end
+				end
+				`EXE_TLT_OP, `EXE_TLTI_OP, `EXE_TLTIU_OP, `EXE_TLTU_OP:		begin
+					if( reg1_lt_reg2 ) begin
+						trapassert <= `TrapAssert;
+					end
+				end
+				`EXE_TNE_OP, `EXE_TNEI_OP:		begin
+					if( reg1_i != reg2_i ) begin
+						trapassert <= `TrapAssert;
+					end
+				end
+				default:				begin
+					trapassert <= `TrapNotAssert;
+				end
+			endcase
+		end
+	end
+
 	assign opdata1_mult = (((aluop_i == `EXE_MUL_OP) || (aluop_i == `EXE_MULT_OP) ||
 													(aluop_i == `EXE_MADD_OP) || (aluop_i == `EXE_MSUB_OP))
 													&& (reg1_i[31] == 1'b1)) ? (~reg1_i + 1) : reg1_i;
 
-  assign opdata2_mult = (((aluop_i == `EXE_MUL_OP) || (aluop_i == `EXE_MULT_OP) ||
+	assign opdata2_mult = (((aluop_i == `EXE_MUL_OP) || (aluop_i == `EXE_MULT_OP) ||
 													(aluop_i == `EXE_MADD_OP) || (aluop_i == `EXE_MSUB_OP))
 													&& (reg2_i[31] == 1'b1)) ? (~reg2_i + 1) : reg2_i;	
 
-  assign hilo_temp = opdata1_mult * opdata2_mult;																				
+	assign hilo_temp = opdata1_mult * opdata2_mult;
 
 	always @ (*) begin
 		if(rst == `RstEnable) begin
@@ -241,11 +272,10 @@ module ex(
 			  mulres <= hilo_temp;
 			end
 		end else begin
-				mulres <= hilo_temp;
+			mulres <= hilo_temp;
 		end
 	end
 
-  //得到最新的HI、LO寄存器的值，此处要解决指令数据相关问题
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			{HI,LO} <= {`ZeroWord,`ZeroWord};
@@ -254,15 +284,14 @@ module ex(
 		end else if(wb_whilo_i == `WriteEnable) begin
 			{HI,LO} <= {wb_hi_i,wb_lo_i};
 		end else begin
-			{HI,LO} <= {hi_i,lo_i};			
+			{HI,LO} <= {hi_i,lo_i};
 		end
-	end	
+	end
 
-  always @ (*) begin
-    stallreq = stallreq_for_madd_msub || stallreq_for_div;
-  end
+	always @ (*) begin
+			stallreq = stallreq_for_madd_msub || stallreq_for_div;
+	end
 
-  //MADD、MADDU、MSUB、MSUBU指令
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			hilo_temp_o <= {`ZeroWord,`ZeroWord};
@@ -271,172 +300,183 @@ module ex(
 		end else begin
 			
 			case (aluop_i) 
-				`EXE_MADD_OP, `EXE_MADDU_OP:		begin
+				`EXE_MADD_OP, `EXE_MADDU_OP: begin
 					if(cnt_i == 2'b00) begin
 						hilo_temp_o <= mulres;
 						cnt_o <= 2'b01;
 						stallreq_for_madd_msub <= `Stop;
 						hilo_temp1 <= {`ZeroWord,`ZeroWord};
 					end else if(cnt_i == 2'b01) begin
-						hilo_temp_o <= {`ZeroWord,`ZeroWord};						
+						hilo_temp_o <= {`ZeroWord,`ZeroWord};
 						cnt_o <= 2'b10;
 						hilo_temp1 <= hilo_temp_i + {HI,LO};
 						stallreq_for_madd_msub <= `NoStop;
 					end
 				end
-				`EXE_MSUB_OP, `EXE_MSUBU_OP:		begin
+				`EXE_MSUB_OP, `EXE_MSUBU_OP: begin
 					if(cnt_i == 2'b00) begin
 						hilo_temp_o <=  ~mulres + 1 ;
 						cnt_o <= 2'b01;
 						stallreq_for_madd_msub <= `Stop;
 					end else if(cnt_i == 2'b01)begin
-						hilo_temp_o <= {`ZeroWord,`ZeroWord};						
+						hilo_temp_o <= {`ZeroWord,`ZeroWord};
 						cnt_o <= 2'b10;
 						hilo_temp1 <= hilo_temp_i + {HI,LO};
 						stallreq_for_madd_msub <= `NoStop;
-					end				
+					end
 				end
 				default:	begin
 					hilo_temp_o <= {`ZeroWord,`ZeroWord};
 					cnt_o <= 2'b00;
-					stallreq_for_madd_msub <= `NoStop;				
+					stallreq_for_madd_msub <= `NoStop;
 				end
 			endcase
 		end
 	end	
 
-  //DIV、DIVU指令	
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			stallreq_for_div <= `NoStop;
-	    div_opdata1_o <= `ZeroWord;
+			div_opdata1_o <= `ZeroWord;
 			div_opdata2_o <= `ZeroWord;
 			div_start_o <= `DivStop;
 			signed_div_o <= 1'b0;
 		end else begin
 			stallreq_for_div <= `NoStop;
-	    div_opdata1_o <= `ZeroWord;
+			div_opdata1_o <= `ZeroWord;
 			div_opdata2_o <= `ZeroWord;
 			div_start_o <= `DivStop;
-			signed_div_o <= 1'b0;	
+			signed_div_o <= 1'b0;
 			case (aluop_i) 
 				`EXE_DIV_OP:		begin
 					if(div_ready_i == `DivResultNotReady) begin
-	    			div_opdata1_o <= reg1_i;
+						div_opdata1_o <= reg1_i;
 						div_opdata2_o <= reg2_i;
 						div_start_o <= `DivStart;
 						signed_div_o <= 1'b1;
 						stallreq_for_div <= `Stop;
 					end else if(div_ready_i == `DivResultReady) begin
-	    			div_opdata1_o <= reg1_i;
+						div_opdata1_o <= reg1_i;
 						div_opdata2_o <= reg2_i;
 						div_start_o <= `DivStop;
 						signed_div_o <= 1'b1;
 						stallreq_for_div <= `NoStop;
-					end else begin						
-	    			div_opdata1_o <= `ZeroWord;
+					end else begin
+						div_opdata1_o <= `ZeroWord;
 						div_opdata2_o <= `ZeroWord;
 						div_start_o <= `DivStop;
 						signed_div_o <= 1'b0;
 						stallreq_for_div <= `NoStop;
-					end					
+					end
 				end
 				`EXE_DIVU_OP:		begin
 					if(div_ready_i == `DivResultNotReady) begin
-	    			div_opdata1_o <= reg1_i;
+						div_opdata1_o <= reg1_i;
 						div_opdata2_o <= reg2_i;
 						div_start_o <= `DivStart;
 						signed_div_o <= 1'b0;
 						stallreq_for_div <= `Stop;
 					end else if(div_ready_i == `DivResultReady) begin
-	    			div_opdata1_o <= reg1_i;
+						div_opdata1_o <= reg1_i;
 						div_opdata2_o <= reg2_i;
 						div_start_o <= `DivStop;
 						signed_div_o <= 1'b0;
 						stallreq_for_div <= `NoStop;
-					end else begin						
-	    			div_opdata1_o <= `ZeroWord;
+					end else begin
+						div_opdata1_o <= `ZeroWord;
 						div_opdata2_o <= `ZeroWord;
 						div_start_o <= `DivStop;
 						signed_div_o <= 1'b0;
 						stallreq_for_div <= `NoStop;
-					end					
+					end
 				end
 				default: begin
 				end
 			endcase
 		end
-	end	
+	end
 
-	//MFHI、MFLO、MOVN、MOVZ指令
 	always @ (*) begin
 		if(rst == `RstEnable) begin
-	  	moveres <= `ZeroWord;
-	  end else begin
-	   moveres <= `ZeroWord;
-	   case (aluop_i)
-	   	`EXE_MFHI_OP:		begin
-	   		moveres <= HI;
-	   	end
-	   	`EXE_MFLO_OP:		begin
-	   		moveres <= LO;
-	   	end
-	   	`EXE_MOVZ_OP:		begin
-	   		moveres <= reg1_i;
-	   	end
-	   	`EXE_MOVN_OP:		begin
-	   		moveres <= reg1_i;
-	   	end
-	   	default : begin
-	   	end
-	   endcase
-	  end
+			moveres <= `ZeroWord;
+		end else begin
+			moveres <= `ZeroWord;
+			case (aluop_i)
+			`EXE_MFHI_OP:		begin
+				moveres <= HI;
+			end
+			`EXE_MFLO_OP:		begin
+				moveres <= LO;
+			end
+			`EXE_MOVZ_OP:		begin
+				moveres <= reg1_i;
+			end
+			`EXE_MOVN_OP:		begin
+				moveres <= reg1_i;
+			end
+			`EXE_MFC0_OP:		begin
+				cp0_reg_read_addr_o <= inst_i[15:11];
+				moveres <= cp0_reg_data_i;
+				if( mem_cp0_reg_we == `WriteEnable &&
+									mem_cp0_reg_write_addr == inst_i[15:11] ) begin
+					moveres <= mem_cp0_reg_data;
+				end else if( wb_cp0_reg_we == `WriteEnable &&
+									wb_cp0_reg_write_addr == inst_i[15:11] ) begin
+					moveres <= wb_cp0_reg_data;
+				end
+			end	   	
+			default : begin
+			end
+			endcase
+		end
 	end	 
 
- always @ (*) begin
-	 wd_o <= wd_i;
+	always @ (*) begin
+		wd_o <= wd_i;
 	 	 	 	
-	 if(((aluop_i == `EXE_ADD_OP) || (aluop_i == `EXE_ADDI_OP) || 
-	      (aluop_i == `EXE_SUB_OP)) && (ov_sum == 1'b1)) begin
-	 	wreg_o <= `WriteDisable;
-	 end else begin
-	  wreg_o <= wreg_i;
-	 end
+		if(((aluop_i == `EXE_ADD_OP) || (aluop_i == `EXE_ADDI_OP) || 
+				(aluop_i == `EXE_SUB_OP)) && (ov_sum == 1'b1)) begin
+			wreg_o <= `WriteDisable;
+			ovassert <= 1'b1;
+		end else begin
+			wreg_o <= wreg_i;
+			ovassert <= 1'b0;
+		end
 	 
-	 case ( alusel_i ) 
-	 	`EXE_RES_LOGIC:		begin
-	 		wdata_o <= logicout;
-	 	end
-	 	`EXE_RES_SHIFT:		begin
-	 		wdata_o <= shiftres;
-	 	end	 	
-	 	`EXE_RES_MOVE:		begin
-	 		wdata_o <= moveres;
-	 	end	 	
-	 	`EXE_RES_ARITHMETIC:	begin
-	 		wdata_o <= arithmeticres;
-	 	end
-	 	`EXE_RES_MUL:		begin
-	 		wdata_o <= mulres[31:0];
-	 	end	 	
-	 	`EXE_RES_JUMP_BRANCH:	begin
-	 		wdata_o <= link_address_i;
-	 	end	 	
-	 	default:					begin
-	 		wdata_o <= `ZeroWord;
-	 	end
-	 endcase
- end	
+		case ( alusel_i ) 
+			`EXE_RES_LOGIC:		begin
+				wdata_o <= logicout;
+			end
+			`EXE_RES_SHIFT:		begin
+				wdata_o <= shiftres;
+			end	 	
+			`EXE_RES_MOVE:		begin
+				wdata_o <= moveres;
+			end	 	
+			`EXE_RES_ARITHMETIC:	begin
+				wdata_o <= arithmeticres;
+			end
+			`EXE_RES_MUL:		begin
+				wdata_o <= mulres[31:0];
+			end	 	
+			`EXE_RES_JUMP_BRANCH:	begin
+				wdata_o <= link_address_i;
+			end	 	
+			default:					begin
+				wdata_o <= `ZeroWord;
+			end
+		endcase
+	end	
 
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			whilo_o <= `WriteDisable;
 			hi_o <= `ZeroWord;
-			lo_o <= `ZeroWord;		
+			lo_o <= `ZeroWord;
 		end else if((aluop_i == `EXE_MULT_OP) || (aluop_i == `EXE_MULTU_OP)) begin
 			whilo_o <= `WriteEnable;
 			hi_o <= mulres[63:32];
-			lo_o <= mulres[31:0];			
+			lo_o <= mulres[31:0];
 		end else if((aluop_i == `EXE_MADD_OP) || (aluop_i == `EXE_MADDU_OP)) begin
 			whilo_o <= `WriteEnable;
 			hi_o <= hilo_temp1[63:32];
@@ -444,11 +484,11 @@ module ex(
 		end else if((aluop_i == `EXE_MSUB_OP) || (aluop_i == `EXE_MSUBU_OP)) begin
 			whilo_o <= `WriteEnable;
 			hi_o <= hilo_temp1[63:32];
-			lo_o <= hilo_temp1[31:0];		
+			lo_o <= hilo_temp1[31:0];
 		end else if((aluop_i == `EXE_DIV_OP) || (aluop_i == `EXE_DIVU_OP)) begin
 			whilo_o <= `WriteEnable;
 			hi_o <= div_result_i[63:32];
-			lo_o <= div_result_i[31:0];							
+			lo_o <= div_result_i[31:0];
 		end else if(aluop_i == `EXE_MTHI_OP) begin
 			whilo_o <= `WriteEnable;
 			hi_o <= reg1_i;
@@ -461,7 +501,23 @@ module ex(
 			whilo_o <= `WriteDisable;
 			hi_o <= `ZeroWord;
 			lo_o <= `ZeroWord;
-		end				
-	end			
+		end
+	end
+
+	always @ (*) begin
+		if(rst == `RstEnable) begin
+			cp0_reg_write_addr_o <= 5'b00000;
+			cp0_reg_we_o <= `WriteDisable;
+			cp0_reg_data_o <= `ZeroWord;
+		end else if(aluop_i == `EXE_MTC0_OP) begin
+			cp0_reg_write_addr_o <= inst_i[15:11];
+			cp0_reg_we_o <= `WriteEnable;
+			cp0_reg_data_o <= reg1_i;
+	  end else begin
+			cp0_reg_write_addr_o <= 5'b00000;
+			cp0_reg_we_o <= `WriteDisable;
+			cp0_reg_data_o <= `ZeroWord;
+		end
+	end
 
 endmodule
