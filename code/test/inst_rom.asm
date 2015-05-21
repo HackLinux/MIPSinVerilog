656dd4ac01b323daf8ab54b4b543624e4cb0d4fd
haoozi <lkwywhy@163.com>
2015-05-21 10:27:45 +0800

    bootloader

--- code/test/inst_rom.asm
+++ code/test/inst_rom.asm
@@ -8,17 +8,17 @@ Disassembly of section .text:
 30000004:	34210008 	ori	at,at,0x8
 30000008:	3c02ffff 	lui	v0,0xffff
 3000000c:	3442ffff 	ori	v0,v0,0xffff
-30000010:	ac020000 	sw	v0,0(zero)
+30000010:	ac220000 	sw	v0,0(at)
 30000014:	3c012000 	lui	at,0x2000
 30000018:	3421000c 	ori	at,at,0xc
 3000001c:	3c020000 	lui	v0,0x0
 30000020:	34420000 	ori	v0,v0,0x0
-30000024:	ac020000 	sw	v0,0(zero)
+30000024:	ac220000 	sw	v0,0(at)
 30000028:	3c012000 	lui	at,0x2000
 3000002c:	34210004 	ori	at,at,0x4
 30000030:	3c025555 	lui	v0,0x5555
 30000034:	34425555 	ori	v0,v0,0x5555
-30000038:	ac020000 	sw	v0,0(zero)
+30000038:	ac220000 	sw	v0,0(at)
 Disassembly of section .reginfo:
 
 00000000 <.reginfo>:
